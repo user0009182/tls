627168d12d4895ef7503195017d1cd45ccaf3db7
user0009182 <80155453+user0009182@users.noreply.github.com>
2022-10-29 14:09:11 +0100

    add hostname verification

--- start_client.bat
+++ start_client.bat
@@ -1,6 +1,7 @@
 REM connects test TLS client using generated certificates
 set path=%path%;D:\old\Program Files\Git\usr\bin
-REM-connect               connect host:port
-REM-cert                  client certificate to verify self with server
+REM -connect                connect host:port
+REM -cert                   client certificate to verify self with server
+REM -verify_hostname        server certificate hostname must match
 set path=%path%;D:\old\Program Files\Git\usr\bin
-openssl s_client -connect localhost:10101 -CAfile certificates\cacert.pem -cert certificates\clientcert.pem -key certificates\clientkey.pem
+openssl s_client -connect localhost:10101 -CAfile certificates\cacert.pem -cert certificates\clientcert.pem -key certificates\clientkey.pem -verify_hostname test.com
--- start_server.bat
+++ start_server.bat
@@ -3,5 +3,6 @@ set path=%path%;D:\old\Program Files\Git\usr\bin
 REM-accept                listen port
 REM-Verify 1              client must supply client certificate
 REM-CAfile                CA certificate used to verify client certificate
+REM-verify_hostname       client certificate hostname must match
 REM-verify_return_error   disconnects client on verification error
-openssl s_server -accept 10101 -Verify 1 -key certificates\serverkey.pem -cert certificates\servercert.pem -CAfile certificates\cacert.pem -verify_return_error
+openssl s_server -accept 10101 -Verify 1 -key certificates\serverkey.pem -cert certificates\servercert.pem -CAfile certificates\cacert.pem -verify_hostname client.com -verify_return_error
